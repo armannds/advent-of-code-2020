ceb47619f1c89c0f5ee3575672c4f79aef909c90
Ármann Davíð Sigurðsson <armann.sigurdsson@marel.com>
2021-01-02 21:14:45 +0000

    First solution

---
+++ AdventOfCode2020/Solutions.Tests/Day01/SolutionTests.cs
@@ -0,0 +1,53 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Solution1Tests.cs" company="Marel hf.">
+// Copyright (c) Marel hf. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Solutions.Tests.Day01
+{
+    using Shouldly;
+    using Xunit;
+    using Day01 = Solutions.Day01;
+
+    public class SolutionTests
+    {
+        [Fact]
+        public void Part1_Should_Return_514579_For_Given_Input()
+        {
+            // Arrange
+            const string input = @"1721
+979
+366
+299
+675
+1456";
+            var day01 = new Day01(input);
+            
+            // Act
+            var result = day01.Solve_1();
+
+            // Assert
+            result.ShouldBe("514579");
+        }
+
+        [Fact]
+        public void Part2_Should_Return_241861950_For_Given_Input()
+        {
+            // Arrange
+            const string input = @"1721
+979
+366
+299
+675
+1456";
+            var day01 = new Day01(input);
+            
+            // Act
+            var result = day01.Solve_2();
+
+            // Assert
+            result.ShouldBe("241861950");
+        }
+    }
+}
---
+++ AdventOfCode2020/Solutions/Day01.cs
@@ -0,0 +1,77 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Day01.cs" company="Marel hf.">
+// Copyright (c) Marel hf. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace Solutions
+{
+    using System.IO;
+    using System.Linq;
+    using AoCHelper;
+
+    public class Day01 : BaseDay
+    {
+        private readonly string input;
+        private const int SumTargetNumber = 2020;
+
+
+        public Day01()
+        {
+            this.input = File.ReadAllText(InputFilePath);
+        }
+
+        public Day01(string input)
+        {
+            this.input = input;
+        }
+
+        public override string Solve_1()
+        {
+            var numbers = this.GetNumbersFromInput();
+
+            for (var i = 0; i < numbers.Length - 1; i++)
+            {
+                for (var j = i + 1; j < numbers.Length; j++)
+                {
+                    var sumOfValues = numbers[i] + numbers[j];
+                    if (sumOfValues == SumTargetNumber)
+                    {
+                        return (numbers[i] * numbers[j]).ToString();
+                    }
+                }
+            }
+
+            return "0";
+        }
+
+        public override string Solve_2()
+        {
+            var numbers = this.GetNumbersFromInput();
+
+            for (var i = 0; i < numbers.Length - 2; i++)
+            {
+                for (var j = i + 1; j < numbers.Length - 1; j++)
+                {
+                    for (var k = j + 2; k < numbers.Length; k++)
+                    {
+                        var sumOfValues = numbers[i] + numbers[j] + numbers[k];
+                        if (sumOfValues == SumTargetNumber)
+                        {
+                            return (numbers[i] * numbers[j] * numbers[k]).ToString();
+                        }
+                        
+                    }
+                }
+            }
+
+            return "0";
+        }
+        
+        private int[] GetNumbersFromInput() => this.input
+            .Split("\n")
+            .Select(_ => _.Trim())
+            .Select(int.Parse)
+            .ToArray();
+    }
+}
